47fc9b9823462dde0ae3e3f03c82c3b75dc115e3
leahy089 <leahy089@morris.umn.edu>
2019-10-01 11:16:58 -0500

    Fix memory leaks in array_merge_test.cpp

--- array_merge/array_merge_test.cpp
+++ array_merge/array_merge_test.cpp
@@ -18,6 +18,8 @@ TEST(ArrayMerge, Handle_empty_list) {
 
   result = array_merge(0, sizes,  a);
   arrays_match(1, result, expected);
+  free(result);
+
 }
 
 TEST(ArrayMerge, Handle_singleton_list) {
@@ -30,6 +32,8 @@ TEST(ArrayMerge, Handle_singleton_list) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(2, result, expected);
+ 
+  free(result);
 }
 
 TEST(ArrayMerge, Handle_one_longer_list) {
@@ -42,6 +46,9 @@ TEST(ArrayMerge, Handle_one_longer_list) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(8, result, expected);
+  
+  free(result);
+
 }
 
 TEST(ArrayMerge, Handle_multiple_copies_of_longer_list) {
@@ -54,6 +61,9 @@ TEST(ArrayMerge, Handle_multiple_copies_of_longer_list) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(8, result, expected);
+  
+  free(result);
+
 }
 
 TEST(ArrayMerge, Handle_multiple_copies_of_longer_list_different_orders) {
@@ -68,6 +78,9 @@ TEST(ArrayMerge, Handle_multiple_copies_of_longer_list_different_orders) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(8, result, expected);
+ 
+  free(result);
+
 }
 
 TEST(ArrayMerge, Handle_different_sizes) {
@@ -88,6 +101,10 @@ TEST(ArrayMerge, Handle_different_sizes) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(11, result, expected);
+  for(i=num_arrays-1; i>=0; --i){
+    free(a[i]);
+  }
+  free(result);
 }
 
 TEST(ArrayMerge, Handle_different_sizes_reversed) {
@@ -108,6 +125,10 @@ TEST(ArrayMerge, Handle_different_sizes_reversed) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(11, result, expected);
+  for(i=num_arrays-1; i>=0; --i){
+    free(a[i]);
+  }
+  free(result);
 }
 
 int main(int argc, char* argv[]) {
